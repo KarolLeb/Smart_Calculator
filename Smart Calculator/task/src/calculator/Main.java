2810d68472b7ce2f17f26062bc843049d0dc0e0d
Karol Lebiodzik <karol.lebiodzik@gmail.com>
2020-07-14 21:03:20 +0200

    Partially changed into RPN

--- Smart Calculator/task/src/calculator/Main.java
+++ Smart Calculator/task/src/calculator/Main.java
@@ -1,14 +1,16 @@
 package calculator;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
+import java.util.Stack;
 
-static class calcState {
-  String activityText;
-  String errorText;
-  String line;
+class calcState {
   int result;
   HashMap<String, Integer> vars;
+  private String errorText;
+  private String line;
+  private String activityText;
 
   calcState() {
     activityText = "";
@@ -18,10 +20,43 @@ static class calcState {
     vars = new HashMap<>();
   }
 
+  String getActivityText() {
+    return activityText;
+  }
+
+  void setActivityText(String text) {
+    activityText = text;
+  }
+
+
+  void cutFirstChar() {
+    line = line.substring(1);
+  }
+
+  void cutWhiteChars() {
+    line = line.replaceAll("\\s+", "");
+  }
+
   void reset() {
     activityText = "";
     errorText = "";
     result = 0;
+  }
+
+  public String getErrorText() {
+    return errorText;
+  }
+
+  public void setErrorText(String errorText) {
+    this.errorText = errorText;
+  }
+
+  public String getLine() {
+    return line;
+  }
+
+  public void setLine(String line) {
+    this.line = line;
   }
 }
 
@@ -30,12 +65,13 @@ public class Main {
     Scanner scanner = new Scanner(System.in);
     calcState state = new calcState();
 
-    while (!state.activityText.equals("Exit command")) {
+    while (!state.getActivityText().equals("Exit command")) {
       state.reset();
-      state.line = scanner.nextLine();
-      if (!state.line.equals("")) {
-        if (state.line.charAt(0) != '/') {
-          if (state.line.contains("=")) {
+      state.setLine(scanner.nextLine());
+      state.cutWhiteChars();
+      if (!state.getLine().equals("")) {
+        if (state.getLine().charAt(0) != '/') {
+          if (state.getLine().contains("=")) {
             assignment(state);
           } else {
             operation(state);
@@ -46,13 +82,13 @@ public class Main {
       } else {
         emptyLine(state);
       }
-      if (state.errorText.equals("")) {
-        if (state.activityText.equals("Operation"))
+      if (state.getErrorText().equals("")) {
+        if (state.getActivityText().equals("Operation"))
           System.out.println(state.result);
-        if (state.activityText.equals("Help command"))
+        if (state.getActivityText().equals("Help command"))
           helpDisplay();
       } else {
-        System.out.println(state.errorText);
+        System.out.println(state.getErrorText());
       }
     }
     System.out.println("Bye!");
@@ -65,95 +101,98 @@ public class Main {
   }
 
   private static void assignment(calcState state) {
-    state.activityText = "Assignment";
-    state.line = state.line.replaceAll("\\s+", "");
-    String[] assignment = state.line.split("=");
+    state.setActivityText("Assignment");
+    String[] assignment = state.getLine().split("=");
 
     if (assignment.length == 2) {
       if (assignment[0].matches("^[a-zA-Z]+$")) {
 
-        if (assignment[1].matches("^\\d+$")) {
+        if (assignment[1].matches("^-?\\d+$")) {
           state.vars.put(assignment[0], Integer.parseInt(assignment[1]));
         } else if (assignment[1].matches("^[a-zA-Z]+$")) {
           if (state.vars.containsKey(assignment[1])) {
             state.vars.put(assignment[0], state.vars.get(assignment[1]));
           }
         } else {
-          state.errorText = "Invalid assignment";
+          state.setErrorText("Invalid assignment");
         }
       } else {
-        state.errorText = "Invalid identifier";
+        state.setErrorText("Invalid identifier");
       }
     } else {
-      state.errorText = "Invalid assignment";
+      state.setErrorText("Invalid assignment");
     }
   }
 
   private static void command(calcState state) {
-    if (state.line.equals("/exit")) {
-      state.activityText = "Exit command";
-    } else if (state.line.equals("/help")) {
-      state.activityText = "Help command";
+    if (state.getLine().equals("/exit")) {
+      state.setActivityText("Exit command");
+    } else if (state.getLine().equals("/help")) {
+      state.setActivityText("Help command");
     } else {
-      state.errorText = "Unknown command";
+      state.setErrorText("Unknown command");
     }
-  }
-
-  private static String simplifyOp(String op) {
-    if (op.charAt(0) == '+') {
-      return "+";
-    }
-    if (op.charAt(0) == '-') {
-      if (op.length() % 2 == 0) {
-        return "+";
-      } else return "-";
-    }
-    return "";
   }
 
   private static void operation(calcState state) {
-    state.activityText = "Operation";
-    state.line = state.line.replaceAll("\\s+", " ");
-    String[] strArr = state.line.split(" ");
-    if (strArr[0].matches("^([+\\-])?(\\d+)$") ||
-      strArr[0].matches("^[a-zA-Z]+$")) {
-      if (strArr[0].matches("^([+\\-])?(\\d+)$")) {
-        state.result = Integer.parseInt(strArr[0]);
-      } else {  // if (strArr[0].matches("^[a-zA-Z]+$"))
-        if (state.vars.containsKey(strArr[0])) {
-          state.result = state.vars.get(strArr[0]);
-        } else {
-          state.errorText = "Unknown variable";
+    state.setActivityText("Operation");
+    ArrayList<String> list = new ArrayList<>();
+    makePostfix(state, list);
+    calculateResult(state, list);
+  }
+
+  private static void calculateResult(calcState state, ArrayList<String> list) {
+    Stack<String> rpn = new Stack<>();
+
+  }
+
+  private static void makePostfix(calcState state, ArrayList<String> list) {
+    Stack<String> rpn = new Stack<>();
+    while (state.getLine().length() > 0) {
+      if (state.getLine().equals("(")) {
+        rpn.push("(");
+        state.cutFirstChar();
+      } else if (state.getLine().equals(")")) {
+        while (!")".equals(rpn.peek())) {
+          list.add(rpn.pop());
         }
+      } else {
+        String[] strArr = state.getLine().split("(?=-) | (?=/+) |" +
+          " (?='/(') | (?='/)') | (?=/*) | (?='//') | (?=^)", 2);
       }
-      for (int i = 1; i < strArr.length; i += 2) {
-        if (i + 1 < strArr.length) {
-          if (strArr[i + 1].matches("^([+\\-])?(\\d+)$")) {
-            state.result = calculate(state, simplifyOp(strArr[i]), Integer.parseInt(strArr[i + 1]));
-          } else {  // if (strArr[0].matches("^[a-zA-Z]+$"))
-            state.result = calculate(state, simplifyOp(strArr[i]), state.vars.get(strArr[i + 1]));
-          }
-        } else {
-          state.errorText = "Invalid expression";
-          break;
-        }
+    }
+    while (rpn.size() > 0) {
+      if (isOperator(rpn.peek())) {
+        list.add(rpn.pop());
       }
-    } else {
-      state.errorText = "Invalid expression";
+      if (isParenthesis(rpn.peek())) {
+
+      }
     }
   }
 
-  private static int calculate(calcState state, String op, int nextNum) {
-    if (op.equals("+")) {
-      return state.result + nextNum;
-    }
-    if (op.equals("-")) {
-      return state.result - nextNum;
-    }
-    return -1;
+  private static boolean isParenthesis(String peek) {
+    return "(".equals(peek) || ")".equals(peek);
+  }
+
+  private static boolean isOperator(String peek) {
+    return ("+".equals(peek)) || ("-".equals(peek)) || ("*".equals(peek)) || ("^".equals(peek)) || ("/".equals(peek));
   }
 
   static void emptyLine(calcState state) {
-    state.activityText = "Empty Line";
+    state.setActivityText("Empty Line");
+  }
+
+  static int precedence(String op) {
+    if (op.equals("+") || op.equals("-")) {
+      return 0;
+    }
+    if (op.equals("/") || op.equals("*")) {
+      return 1;
+    }
+    if (op.equals("^")) {
+      return 2;
+    }
+    return -1;
   }
 }
